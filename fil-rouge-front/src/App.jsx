7029b41e8619a3a5cd26be2864e7e12ec2eebafd
Mehdev9 <mehdi-belkateb@hotmail.com>
2024-11-26 16:15:35 +0100

    installation des dependances packagejson

--- fil-rouge-front/src/App.jsx
+++ fil-rouge-front/src/App.jsx
@@ -1,64 +1,59 @@
-import {BrowserRouter, Navigate, Outlet, Route, Routes} from "react-router-dom";
-import {HomeView} from "./views/HomeView.jsx";
-import {RegisterView} from "./views/RegisterView.jsx";
-import {LoginView} from "./views/LoginView.jsx";
-import {PrivateView} from "./views/PrivateView.jsx";
-import {Navbar} from "./components/Navbar.jsx";
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {login} from "./store/authSlice.js";
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { HomeView } from "./views/HomeView.jsx";
+import { RegisterView } from "./views/RegisterView.jsx";
+import { LoginView } from "./views/LoginView.jsx";
+import { PrivateView } from "./views/PrivateView.jsx";
+import { Navbar } from "./components/Navbar.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { login } from "./store/authSlice.js";
 
 const App = () => {
-
     const dispatch = useDispatch();
 
     useEffect(() => {
         const token = localStorage.getItem("token");
-        if (token){
-            dispatch(login(token))
+        if (token) {
+            dispatch(login(token));
         }
-    },[dispatch]);
+    }, [dispatch]);
 
     return (
         <BrowserRouter>
-            <Navbar/>
+            <Navbar />
             <Routes>
-                <Route path="/" element={<HomeView/>}/>
+                <Route path="/" element={<HomeView />} />
 
-                <Route element={<UnprotectedRoute/>}>
-                    <Route path="/register" element={<RegisterView/>}/>
-                    <Route path="/login" element={<LoginView/>}/>
-                </Route>
-
-                <Route element={<ProtectedRoute/>}>
-                    <Route path="/account" element={<PrivateView/>}/>
+                <Route element={<UnprotectedRoute />}>
+                    <Route path="/register" element={<RegisterView />} />
+                    <Route path="/login" element={<LoginView />} />
                 </Route>
 
 
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/account" element={<PrivateView />} />
+                </Route>
             </Routes>
         </BrowserRouter>
-    )
+    );
+};
 
-}
 export default App;
 
 const ProtectedRoute = () => {
-
-    const isAuthenticated = useSelector(store => store.auth.isAuthenticated);
-
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace/>
+        return <Navigate to="/login" replace />;
     }
-    return <Outlet/>;
-}
+    return <Outlet />;
+};
 
 const UnprotectedRoute = () => {
-
-    const isAuthenticated = useSelector(store => store.auth.isAuthenticated);
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
     if (isAuthenticated) {
-        return <Navigate to="/" replace/>
+        return <Navigate to="/" replace />;
     }
-    return <Outlet/>;
-}
+    return <Outlet />;
+};
---
+++ fil-rouge-front/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css';
+
+const Nav = () => {
+    return (
+        <nav className="nav-bar">
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Accueil</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className="nav-link">A propos</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/services" className="nav-link">Services</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/contact" className="nav-link">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
