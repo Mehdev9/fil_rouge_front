19b4d3988cbe051814cc24bc4ff5ba010f228467
Mehdev9 <mehdi-belkateb@hotmail.com>
2025-01-15 15:26:51 +0100

    dropdown compte added in nav and link in index.html

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import '../css/nav.css';
 import Logo from './Logo.jsx';
 
 const Nav = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        setIsLoggedIn(false);
+    };
+
     return (
         <nav className="nav-bar bg-primary text-light">
             <ul className="nav-list">
@@ -24,6 +39,28 @@ const Nav = () => {
                 </li>
                 <li className="nav-item">
                     <Link to="/contact" className="nav-link">Contact</Link>
+                </li>
+
+                <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Compte
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        {isLoggedIn ? (
+                            <>
+                                <li>
+                                    <Link to="/account" className="dropdown-item">Mon compte</Link>
+                                </li>
+                                <li>
+                                    <button className="dropdown-item" onClick={handleLogout}>Déconnexion</button>
+                                </li>
+                            </>
+                        ) : (
+                            <li>
+                                <Link to="/login" className="dropdown-item">Connexion</Link>
+                            </li>
+                        )}
+                    </ul>
                 </li>
             </ul>
         </nav>
